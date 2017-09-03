8573e2bc60627a33487a0e094d4ae0386d46552a
LautaroOdrizola <lautagomezodriozola@est.frba.utn.edu.ar>
2017-09-03 20:38:28 -0300

    Estructuras basicas

---
+++ FileSystem/src/structsFS.c
@@ -0,0 +1,9 @@
+#include "../../Socket/src/funcionesSocket.c"
+
+
+typedef struct t_directory {
+	int index;
+	char nombre[255];
+	int padre;
+} directorio;
+
---
+++ Master/src/structsMaster.c
@@ -0,0 +1,29 @@
+#include "../../Socket/src/funcionesSocket.c"
+
+#define sizeMaxMsg 128
+
+// No se que nombre ponerles a las rutas de los archivos temporales.
+// Ahora quedan horribles, despues los cambiamos
+
+typedef struct strTransformacion{
+	char* nodo;
+	char* worker_ip;
+	int bloque;
+	long int bytesOcupados;
+	char* temporalesTransformacion;
+} transformacion;
+
+
+typedef struct strReduccionLocal{
+	char* nodo;
+	char* worker_ip;
+	char* temporalesTransformacion; // Pensaba hacer un array de char*. Cada posicion seria una direccion de archivo temporal de transformacion
+	char* temporalesReduccionLocal;
+} reduccionLocal ;
+
+typedef struct strReduccionGlobal{
+	char* nodo;
+	char* worker_ip;
+	char* temporalesReduccionLocal;
+	int encargado; // 1 si es encargado, 0 si no.
+} reduccionGlobal;
