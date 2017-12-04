c00be7bd6b5df3695f285ee51e76329b7f49738b
lromano <lromano@est.frba.utn.edu.ar>
2017-12-03 22:17:06 -0300

    Rippeo readline

--- FileSystem/src/Consola.c
+++ FileSystem/src/Consola.c
@@ -1,13 +1,7 @@
-/*
- * Consola.c
- *
- *  Created on: 25/11/2017
- *      Author: utnso
- */
-
 #include "Consola.h"
 
-#define maxSize 256
+
+uint32_t countAmmount = 12;
 
 typedef struct {
 	int flag;
@@ -15,7 +9,7 @@ typedef struct {
 } command;
 
 
-command comandos[] = {
+command commands[] = {
 	{ 1,"format"},
 	{ 2,"rm"},
 	{ 3,"rename"},
@@ -54,280 +48,163 @@ void imprimirComandos(){
 	puts("info: Mostrar información de un archivo. \n");
 }
 
-
-void analizarComando(char * linea){
-
-	int i;
-	int comandoNativo;
-	int limite = (sizeof(comandos)/sizeof(comandos[0]));
-	command comandoAux;
-
-	char ** comandoDesarmado = string_split(linea," ");
-	char * primerPos = comandoDesarmado[0];
-
-	for(i=0; i < limite; i++){
-
-		comandoAux = comandos[i];
-
-		if( strcmp(primerPos, comandoAux.nombre) == 0){
-			comandoNativo = comandoAux.flag;
+void analizeCommand(char* cmd){
+	char** disarmedCmd = string_split(cmd, " ");
+	char* function = disarmedCmd[0];
+	uint32_t commandNumber;
+	command auxCommand;
+	int count;
+	for(count = 0; count < countAmmount; count++){
+		auxCommand = commands[count];
+		if(strcmp(function, auxCommand.nombre)==0){
+			commandNumber = auxCommand.flag;
 			break;
 		}
-
 	}
 
-
-	switch(comandoNativo){
-
-		case 1:{
-
-			char * comandoNuevo = string_new();
-			printf("Formateando FileSystem.\n");
-
-			if(hayEstadoAnterior==false){
-
-				uint cantidadNodosSistemas=list_size(tablaGlobalNodos->nodo);
-				if(cantidadNodosSistemas>=2){
-					estaFormateado=true;
-					esEstadoSeguro=true;
-				}else{
-					log_error(loggerFileSystem,"No hay suficientes DataNode para dejar el FS en un estado Estable");
-				}
-
-			} else {
-
-				if(hayUnEstadoEstable()){
-					esEstadoSeguro=true;
-				} else {
-					log_error(loggerFileSystem,"No hay al menos una copia de cada archivo. Estado no estable.");
-				}
-
-			}
-
-			free(comandoNuevo);
-
-		}
-        break;
-
-		case 2:{
-
-			if(strcmp(comandoDesarmado[1], "-d") == 0){
-
-
-			} else if(strcmp(comandoDesarmado[1], "-b") == 0){
-
-			} else {
-
-				char * path = comandoDesarmado[1];
-				borrarArchivo(path);
-
-			}
-
-
-		}
-		break;
-
-
-		case 3:{
-			char * comandoNuevo = string_new();
-
-			char * nombreArchivoViejo = comandoDesarmado[1];
-			char * nombreArchivoNuevo = comandoDesarmado[2];
-
-			if(nombreArchivoViejo == NULL || nombreArchivoNuevo == NULL){
-				log_error(loggerFileSystem, "Faltan parametros para ejecutar el comando rename");
-			} else {
-
-				renameDirectory(nombreArchivoViejo,nombreArchivoNuevo);
-
-				string_append(&comandoNuevo,"rename ");
-				string_append(&comandoNuevo,nombreArchivoViejo);
-				string_append(&comandoNuevo," ");
-				string_append(&comandoNuevo,nombreArchivoNuevo);
-
-				system(comandoNuevo);
-				printf("\n");
-
-			}
-
-			free(comandoNuevo);
-
-		}
-		break;
-
-		case 4:{
-			char * comandoNuevo = string_new();
-
-			char * pathOriginal = comandoDesarmado[1];
-			char * pathFinal = comandoDesarmado[2];
-
-			if(pathOriginal == NULL || pathFinal == NULL){
-				log_error(loggerFileSystem, "Faltan parametros para ejecutar el comando mv");
-			} else {
-
-				moveDirectory(pathOriginal,pathFinal);
-
-				string_append(&comandoNuevo,"mv ");
-				string_append(&comandoNuevo,pathOriginal);
-				string_append(&comandoNuevo," ");
-				string_append(&comandoNuevo,pathFinal);
-
-				system(comandoNuevo);
-				printf("\n");
-
-			}
-
-			free(comandoNuevo);
-
-		}
-		break;
-
-		case 5:{
-
-			char * path = comandoDesarmado[1];
-
-			if(existePath(path)){
-				printf("Existe path");
-			} else {
-				printf("No existe path");
-			}
-			printf("\n");
-
-		}
-		break;
-
-		case 6:{
-
-			char* nuevoDirectorio = comandoDesarmado[1];
-
-			if(nuevoDirectorio != NULL){
-				if(!existeDirectory(nuevoDirectorio)){
-					if(crearDirectorio(nuevoDirectorio) == 1){
-						system(linea);
-						printf("\n");
-
-					}else{
-						log_error(loggerFileSystem, "No se pudo crear el directorio. Por favor vuelva a intentarlo");
-					}
-
-				}else{
-					log_error(loggerFileSystem, "No se pudo crear el directorio. El directorio ya existe.");
-				}
-
-			}else{
-				log_error(loggerFileSystem, "Asegurese de ingresar el nombre del directorio. Por favor vuelva a intentarlo");
-			}
-
-		}
-		break;
-
-		case 7:{
-			char * nombreArchivoViejo = comandoDesarmado[1];
-			char * nombreArchivoNuevo = comandoDesarmado[2];
-			char * flag = comandoDesarmado[3];
-			if(nombreArchivoViejo != NULL && nombreArchivoNuevo != NULL && flag != NULL){
-				almacenarArchivo(nombreArchivoViejo,nombreArchivoNuevo,flag);
-			}else{
-				log_error(loggerFileSystem, "Faltan parametros para ejecutar el comando cpfrom");
-			}
-
-		}
-		break;
-
-		case 8:{
-			char * pathArchivoOrigen = comandoDesarmado[1];
-			char * directorioFilesystem = comandoDesarmado[2];
-
-			//copiaArchivoYamafsAlLocal(pathArchivoOrigen,directorioFilesystem);
-
-			printf("Comando en arreglo! Todavia no se puede ejecutar! (8)\n");
-		}
-		break;
-
-		case 9:{
-			//
-			// Ver bien como hacer estos comandos despues
-			//
-			printf("Comando en arreglo! Todavia no se puede ejecutar! (9)\n");
-		}
-		break;
-
-		case 10:{
-			char * comandoNuevo = string_new();
-
-			char * nombreArchivoViejo = comandoDesarmado[1];
-
-			if(nombreArchivoViejo == NULL){
-				log_error(loggerFileSystem, "Faltan parametros para ejecutar el comando md5sum");
-			} else {
-				string_append(&comandoNuevo,"md5sum ");
-				string_append(&comandoNuevo,nombreArchivoViejo);
-				system(comandoNuevo);
-				printf("\n");
-			}
-
-			free(comandoNuevo);
-
-		}
-		break;
-
-		case 11:{
-			system(linea);
-			printf("\n");
-		}
-		break;
-
-		case 12:{
-			char * comandoNuevo = string_new();
-			char * nombreArchivoViejo = string_new();
-
-			string_append(&nombreArchivoViejo, comandoDesarmado[1]);
-			if(nombreArchivoViejo == NULL){
-				log_error(loggerFileSystem, "Faltan parametros para ejecutar el comando info.");
-			} else {
-
-				string_append(&comandoNuevo,"ls -l -h ");
-				string_append(&comandoNuevo,nombreArchivoViejo);
-				system(comandoNuevo);
-				printf("\n");
-
-			}
-
-			free(comandoNuevo);
-			free(nombreArchivoViejo);
-
-		}
-		break;
-
-		default:
-			log_error(loggerFileSystem, "Comando no reconocido.");
-		break;
-
-	}
-
+	switch (commandNumber) {
+    case 1:
+    	if(hayEstadoAnterior==false){
+    		uint cantidadNodosSistemas=list_size(tablaGlobalNodos->nodo);
+    		if(cantidadNodosSistemas>=2){
+    			estaFormateado=true;
+    			esEstadoSeguro=true;
+    		}else{
+    			log_error(loggerFileSystem,"No hay suficientes DataNode para dejar el FS en un estado Estable");
+    		}
+    	} else {
+    		if(hayUnEstadoEstable()){
+    			esEstadoSeguro=true;
+    		} else {
+    			log_error(loggerFileSystem,"No hay al menos una copia de cada archivo. Estado no estable.");
+    		}
+    		log_info(loggerFileSystem, "FileSystem formateado.");
+    	}
+    		break;
+    case 2:
+    	if(strcmp(disarmedCmd[1], "-d") == 0){
+    	} else if(strcmp(disarmedCmd[1], "-b") == 0){
+    	} else {
+    		char * path = disarmedCmd[1];
+    		borrarArchivo(path);
+    	}
+    break;
+    case 3:{
+    	char * nombreArchivoViejo = disarmedCmd[1];
+    	char * nombreArchivoNuevo = disarmedCmd[2];
+    	if(nombreArchivoViejo == NULL || nombreArchivoNuevo == NULL){
+    		log_error(loggerFileSystem, "Faltan parametros para ejecutar el comando rename");
+    	} else {
+    		renameDirectory(nombreArchivoViejo,nombreArchivoNuevo);
+    	}
+    }
+    break;
+    case 4:{
+      char * pathOriginal = disarmedCmd[1];
+      char * pathFinal = disarmedCmd[2];
+      if(pathOriginal == NULL || pathFinal == NULL){
+    	  log_error(loggerFileSystem, "Faltan parametros para ejecutar el comando mv");
+      } else {
+    	  moveDirectory(pathOriginal,pathFinal);
+      }
+    }
+    break;
+    case 5:{
+      char * path = disarmedCmd[1];
+      if(existePath(path)){
+    	  printf("Existe path");
+      } else {
+    	  printf("No existe path");
+      }
+    }
+    break;
+    case 6:{
+      char* nuevoDirectorio = disarmedCmd[1];
+      if(nuevoDirectorio != NULL){
+    	  if(!existeDirectory(nuevoDirectorio)){
+    		  if(crearDirectorio(nuevoDirectorio) != 1){
+    			  log_error(loggerFileSystem, "No se pudo crear el directorio. Por favor vuelva a intentarlo");
+    		  }
+
+    	  }else{
+    		  log_error(loggerFileSystem, "No se pudo crear el directorio. El directorio ya existe.");
+    	  }
+
+      }else{
+    	  log_error(loggerFileSystem, "Asegurese de ingresar el nombre del directorio. Por favor vuelva a intentarlo");
+      }
+    }
+    break;
+    case 7:{
+      char * nombreArchivoViejo = disarmedCmd[1];
+      char * nombreArchivoNuevo = disarmedCmd[2];
+      char * flag = disarmedCmd[3];
+      if(nombreArchivoViejo != NULL && nombreArchivoNuevo != NULL && flag != NULL){
+    	  almacenarArchivo(nombreArchivoViejo,nombreArchivoNuevo,flag);
+      }else{
+    	  log_error(loggerFileSystem, "Faltan parametros para ejecutar el comando cpfrom");
+      }
+    }
+    break;
+    case 8:{
+    	char * pathArchivoOrigen = disarmedCmd[1];
+    	char * directorioFilesystem = disarmedCmd[2];
+    	printf("Comando en arreglo! Todavia no se puede ejecutar! (8)\n");
+    }
+    break;
+    case 9:{}
+
+    break;
+    case 10:{
+    	char* comandoNuevo = string_new();
+    	char * nombreArchivoViejo = disarmedCmd[1];
+    	if(nombreArchivoViejo == NULL){
+    		log_error(loggerFileSystem, "Faltan parametros para ejecutar el comando md5sum");
+    	} else {
+    		string_append(&comandoNuevo,"md5sum ");
+    		string_append(&comandoNuevo,nombreArchivoViejo);
+    		system(comandoNuevo);
+    		printf("\n");
+    	}
+    }
+    break;
+    case 11:{}
+
+    break;
+    case 12:{
+    	char * nombreArchivoViejo = string_new();
+    	string_append(&nombreArchivoViejo, disarmedCmd[1]);
+    	if(nombreArchivoViejo == NULL){
+    		log_error(loggerFileSystem, "Faltan parametros para ejecutar el comando info.");
+    	} else {
+        // string_append(&comandoNuevo,"ls -l -h ");
+        // string_append(&comandoNuevo,nombreArchivoViejo);
+        // system(comandoNuevo);
+        // printf("\n");
+        //VER SI ES NECESARIO EL SYSTEM
+    	}
+    	free(nombreArchivoViejo);
+    }
+    break;
+    default:
+    	log_warning(loggerFileSystem, "Comando no reconocido, por favor vuelva a intentarlo");
+  }
 }
 
 void consolaFS(){
-
-	char * linea;
-	imprimirComandos();
-
-	printf("\n");
-
-	while(1) {
-		linea = (char *) readline(">");
-
-		if(linea)
-			add_history(linea);
-
-		if(!strncmp(linea, "exit", 4)) {
-			pthread_detach(hiloConsolaFS);
-			break;
-		} else {
-			analizarComando(linea);
-		}
-
-	}
-
+  char* command = malloc(100*sizeof(char));
+  size_t maxSize = 100*sizeof(char);
+  while(1){
+    puts("Esperando comando...");
+    printf("->");
+    getline(&command, &maxSize, stdin);
+    uint32_t recvdSize = strlen(command);
+    command[recvdSize-1] = '\0';
+    if(strcmp(command, "exit")==0){
+      free(command);
+      pthread_detach(hiloConsolaFS);
+      break;
+    }
+    analizeCommand(command);
+  }
 }
-
