bef4a78a5972a2e63407ca7548104e13b734dff8
CDNievas <cdnievas@hotmail.com>
2017-12-05 10:45:30 -0300

    Arreglo archivos de compilacion

--- Compilacion/yama.sh
+++ Compilacion/yama.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
 cd ../YAMA/src
 echo "Compilando YAMA"
 gcc balanceoDeCargas.c funcionesYAMA.c reduccionGlobal.c reduccionLocal.c serializaciones.c transformacion.c YAMA.c -o yama -lcommons -lpthread
+cd ../../Compilacion
--- script.sh
+++ script.sh
@@ -1,23 +1,23 @@
+#!/bin/sh
 echo "Instalando commons"
-cd ../
 git clone https://github.com/sisoputnfrba/so-commons-library
-cd so-commons-library
+cd so-commons-library/
 make install
 cd ../
 echo "Instalando readline"
 sudo apt-get install libreadline6 libreadline6-dev
 echo "Compilando Datanode"
-cd tp-2017-2c-ElTPEstaBien/DataNode/src/
+cd DataNode/src/
 gcc funcionesDatanode.c datanode.c -o datanode -lcommons
 echo "Compilando FileSystem"
-cd ../../FileSystem/src
-gcc BitarrayConfiguraciones.c FuncionesDirectorios.c Consola.c EstadoAnterior.c FS.c FuncionesFS.c Nodos.c Persistencia.c Worker.c YAMA.c -o filesystem -lcommons -lpthread -lreadline
+cd ../../FileSystem/src/
+gcc filesystem.c nodos.c principalesFS.c -o filesystem -lcommons -lpthread -lreadline
 echo "Compilando YAMA"
-cd ../../YAMA/src
+cd ../../YAMA/src/
 gcc balanceoDeCargas.c funcionesYAMA.c reduccionGlobal.c reduccionLocal.c serializaciones.c transformacion.c YAMA.c -o yama -lcommons -lpthread
 echo "Compilando Worker"
-cd ../../Worker/src
+cd ../../Worker/src/
 gcc worker.c -o worker -lcommons -lpthread -lm
 echo "Compilando Master"
-cd ../../Master/src
+cd ../../Master/src/
 gcc master.c -o master -lcommons -lpthread -lm
